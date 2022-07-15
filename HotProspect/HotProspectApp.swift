5e68115087ae1d219fe86b2cfc6a6bf9f1080366
Shokri Alnajjar <shokre4@hotmail.com>
2022-07-15 16:33:46 +0400

    Initial Commit

---
+++ HotProspect/HotProspectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectApp.swift
+//  HotProspect
+//
+//  Created by Shokri Alnajjar on 15.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
