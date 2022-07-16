8b893ac9a9f94172b79801b87c345b5edf59fab1
Shokri Alnajjar <shokre4@hotmail.com>
2022-07-16 22:42:31 +0400

    project done

--- HotProspect/ProspectsView.swift
+++ HotProspect/ProspectsView.swift
@@ -6,18 +6,65 @@
 //
 
 import SwiftUI
+import CodeScanner
+import UserNotifications
 
 struct ProspectsView: View {
     enum FilterType {
         case none, contacted, noncontacted
     }
     
+    @EnvironmentObject var prospects: Prospects
+    @State private var isShowingScanner = false
     let filter: FilterType
     
     var body: some View {
         NavigationView {
-        Text("Hello, World!")
+            List {
+                ForEach(filtereProspects) { prospect in
+                    VStack(alignment:.leading){
+                        Text(prospect.name)
+                            .font(.headline)
+                        Text(prospect.emailAddress)
+                            .foregroundColor(.secondary)
+                    }
+                    .swipeActions {
+                        if prospect.isContacted {
+                            Button {
+                                prospects.toggle(prospect)
+                            } label: {
+                                Label("Mark Uncontacted", systemImage: "person.crop.circle.badge.xmark")
+                            }
+                            .tint(.blue)
+                        } else {
+                            Button {
+                                prospects.toggle(prospect)
+                            } label: {
+                                Label("Mark Contacted", systemImage: "person.crop.circle.fill.badge.checkmark")
+                            }
+                            .tint(.green)
+                            
+                            Button {
+                                addNotification(for: prospect)
+                            }label: {
+                                Label("Remind me", systemImage: "bell")
+                            }
+                            .tint(.orange)
+                        }
+                    }
+                }
+            }
                 .navigationTitle(title)
+                .toolbar {
+                    Button {
+                        isShowingScanner = true
+                    } label: {
+                        Label("Scan", systemImage: "qrcode.viewfinder")
+                    }
+                }
+                .sheet(isPresented: $isShowingScanner) {
+                    CodeScannerView(codeTypes: [.qr], simulatedData: "Paul Hudson\npaul@hackingwithswift.com", completion: handleScan)
+                }
         }
     }
     
@@ -32,10 +79,76 @@ struct ProspectsView: View {
         }
         
     }
+    
+    var filtereProspects: [Prospect] {
+        switch filter {
+        case .none:
+            return prospects.people
+        case .contacted:
+            return prospects.people.filter { $0.isContacted}
+        case .noncontacted:
+            return prospects.people.filter {!$0.isContacted}
+        }
+    }
+    
+    func handleScan(result: Result<ScanResult, ScanError>) {
+        isShowingScanner = false
+        
+        switch result {
+        case .success(let result):
+            let details = result.string.components(separatedBy: "\n")
+            guard  details.count == 2 else { return }
+            
+            let person = Prospect()
+            person.name = details[0]
+            person.emailAddress = details[1]
+            
+            prospects.add(person)
+        case .failure(let error):
+            print("Scanning Failed \(error.localizedDescription)")
+            
+        }
+        
+    }
+    
+    func addNotification(for prospect: Prospect){
+        let center = UNUserNotificationCenter.current()
+        
+        let addRequest = {
+            let content = UNMutableNotificationContent()
+            content.title = "Contact \(prospect.name)"
+            content.subtitle =  prospect.emailAddress
+            content.sound = UNNotificationSound.default
+            
+            var dateComponent = DateComponents()
+            dateComponent.hour = 9
+            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponent, repeats: false)
+            //let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+            
+            center.add(request)
+            
+        }
+        
+        center.getNotificationSettings {settings in
+            if settings.authorizationStatus == .authorized {
+                addRequest()
+            } else {
+                center.requestAuthorization(options:[.badge,.sound,.alert]){success,error in
+                    if success {
+                        addRequest()
+                    } else {
+                        print("D'oh!")
+                    }
+                }
+            }
+        }
+    }
 }
 
 struct ProspectsView_Previews: PreviewProvider {
     static var previews: some View {
         ProspectsView(filter:.none)
+            .environmentObject(Prospects())
     }
 }
--- Prospect.swift
+++ Prospect.swift
@@ -11,11 +11,38 @@ class Prospect: Identifiable, Codable {
     var id = UUID()
     var name = "Anonymous"
     var emailAddress = ""
-    var isContacted = false
+    fileprivate(set) var isContacted = false
 }
 
-@MainActor calss Prospects: ObservableObject {
-    Published var people: [Prospect]
+@MainActor class Prospects: ObservableObject {
+    @Published private(set) var people: [Prospect]
+    let saveKey = "SavedData"
+    init() {
+        if let data = UserDefaults.standard.data(forKey: saveKey) {
+            if let decoded = try? JSONDecoder().decode([Prospect].self, from: data) {
+                people = decoded
+                return
+            }
+        }
+        
+        people = []
+    }
     
+    private func save() {
+        if let encoded = try? JSONEncoder().encode(people) {
+            UserDefaults.standard.set(encoded, forKey: saveKey)
+        }
+    }
     
+    func toggle(_ prospect: Prospect) {
+        objectWillChange.send()
+        prospect.isContacted.toggle()
+        save()
+    }
+    
+    func add(_ prospect: Prospect) {
+        people.append(prospect)
+        save()
+    }
+     
 }
